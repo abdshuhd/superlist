a6ea970c29c801c29a6618e5b062e975590e69db
Bau <qru@gmail.com>
2026-05-13 14:42:41 +0800

    message 22: use Bootstarp to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,10 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver.common.by import By
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.common.exceptions import NoSuchElementException
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser=webdriver.Chrome()
     def tearDown(self):
@@ -27,10 +27,10 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024,768)
         #他新建一个清单，看到输入框完美地居中显示
+        inputbox=self.browser.find_element(By.ID,'id_new_item')
         inputbox.send_keys('testing')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: testing')
-        inputbox=self.browser.find_element(By.ID,'id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x']+inputbox.size['width']/2,
             512,
